ba21ca71349f8894ec3f01ce0bca04626efc66b0
ijabotristan <ijabotristan1@gmail.com>
2026-08-25 18:21:05 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -16,4 +16,6 @@ app.get('/health', (req, res) => res.json({ status: 'ok' }));
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`SMS backend running on port ${PORT}`));
 
-module.exports = app;
+module.exports = app; app.get('/', (req, res) => {
+  res.send('School Management System API is running 🚀');
+});
